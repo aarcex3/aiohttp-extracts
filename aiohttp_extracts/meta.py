d07abe1001a83cd2e8f13c7db30ff44b3291c4f5
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-10 13:58:46 -0400

    refactor (ParameterMeta): remove unused lines

--- aiohttp_extracts/meta.py
+++ aiohttp_extracts/meta.py
@@ -1,8 +1,6 @@
 from abc import ABCMeta
 from typing import Any, Dict, Iterable, Union
 
-from aiohttp import web
-
 
 class ParameterMeta(ABCMeta):
     def __init__(self, *args, **kwargs):
@@ -13,13 +11,3 @@ class ParameterMeta(ABCMeta):
         default_attrs.update(cls.__parse_key__(key))
 
         return type(cls.__name__, (cls,), default_attrs)
-
-    def __parse_key__(cls, key: Union[str, type, Iterable]) -> Dict[str, Any]:
-        params: Dict[str, Any] = {}
-        if isinstance(key, str):
-            params["name"] = key
-        elif isinstance(key, type):
-            params["type"] = key
-        elif isinstance(key, Iterable):
-            params.update({k: v for k, v in zip(("name", "type"), key)})
-        return params
